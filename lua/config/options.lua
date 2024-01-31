6a2ff3230bad07dd9e66ea8d2bcd55c186d9fc62
zonggui <15600598920@163.com>
2024-01-31 11:37:48 +0800

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- neovide 设置
+--
+vim.g.neovide_scale_factor = 0.8 -- neovide 的缩放因子，
+vim.g.neovide_background_alpha = 0.5
